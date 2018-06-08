b51a3e7e2bcbbc5a35055d05cc9f4d041debfaca
Joel Myers <myersoftsolutions@gmail.com>
2018-06-08 00:17:43 -0400

    CitySearchHomeViewController added and Storyboard ViewController preliminary wire up

---
+++ 3ARTHCitySearch/3ARTHCitySearch/CitySearchHomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CitySearchHomeViewController.swift
+//  3ARTHCitySearch
+//
+//  Created by Joel Myers on 6/7/18.
+//  Copyright © 2018 4Backbase. All rights reserved.
+//
+
+import UIKit
+
+class CitySearchHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
