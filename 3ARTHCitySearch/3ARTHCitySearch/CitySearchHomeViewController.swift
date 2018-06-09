2ff5b92be6da8a184bf90e4e6e5a7bef6cd887ee
Joel Myers <myersoftsolutions@gmail.com>
2018-06-09 13:55:46 -0400

    UISearchBar Delegate method added for filtering citiesArray; first pass

--- 3ARTHCitySearch/3ARTHCitySearch/CitySearchHomeViewController.swift
+++ 3ARTHCitySearch/3ARTHCitySearch/CitySearchHomeViewController.swift
@@ -15,11 +15,14 @@ class CitySearchTableViewCell: UITableViewCell {
 
 class CitySearchHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
+
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
-    var citiesArray : [Any]?
-
+    var citiesArray : [City]?
+    var isSearching = false
+    
     let jsonHandler = JSONHandler.defaultHandler
+    var filteredCities : [City] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,9 +34,46 @@ class CitySearchHomeViewController: UIViewController, UITableViewDelegate, UITab
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    //MARK: Filtering Methods for search
+    func isFiltering() -> Bool {
+        return !searchBarIsEmpty()
+    }
+
+    func searchBarIsEmpty() -> Bool {
+        return searchBar.text?.isEmpty ?? true
+    }
+    //MARK: UISearchBarDelegate Method for filtering search text
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == ""{
+            isSearching = false
+//            view.endEditing(true)
+            tableView.reloadData()
+        } else{
+            isSearching = true
+            filteredCities = (citiesArray?.filter {
+                $0.name.lowercased().contains(searchText.lowercased())
+                })!
+                
+            tableView.reloadData()
+        }
+    }
+    
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+//        filteredCities = citiesArray!.filter({( city : City) -> Bool in
+//            return (city ).name.lowercased().contains(searchText.lowercased())
+//            } as! (Any) -> Bool) as! [City]
+//
+//        tableView.reloadData()
+//        filteredCities = Array.filter {
+//            $0.name == searchText
+//        }
+    }
     
     //MARK: UITableView Datasource & Delegate Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearching {
+            return filteredCities.count
+        }
         return (citiesArray?.count)!
     }
 
@@ -43,7 +83,13 @@ class CitySearchHomeViewController: UIViewController, UITableViewDelegate, UITab
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CitySearchTableViewCell", for: indexPath) as! CitySearchTableViewCell
-        let city = citiesArray?[indexPath.row] as! City
+        var city : City
+        
+        if isSearching {
+            city = filteredCities[indexPath.row]
+        } else {
+            city = (citiesArray?[indexPath.row])!
+        }
         cell.nameLabel.text = "\(city.name), \(city.country)"
         
         return cell
@@ -54,7 +100,7 @@ class CitySearchHomeViewController: UIViewController, UITableViewDelegate, UITab
             let vc = segue.destination as! CityMapViewController
             let cell = sender as! CitySearchTableViewCell
             let idxPath = tableView.indexPath(for: cell)
-            vc.city = citiesArray?[(idxPath?.row)!] as? City
+            vc.city = citiesArray?[(idxPath?.row)!]
         }
     }
 
